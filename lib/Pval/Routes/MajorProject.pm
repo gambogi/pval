gambogi/pval

commit be2ddbbbfe005af690f4ed8b000245390ea5de0b

added aggregate result route for major projects

lib/Pval/Routes/MajorProject.pm
@@ -40,12 +40,24 @@ get '/incoming' => sub {
     aggregate_projects { status => 'pending' };
 };
 
+get '/:result' => sub {
+    aggregate_projects {status => param 'result'};
+};
+
 get '/year/:year' => sub {
     return {
         error => 'Invalid year '.param 'year'
     }, 'error', request->content_type unless valid_year param 'year';
     aggregate_projects {}, param 'year';
 };
+
+get '/year/:year/:result' => sub {
+    return {
+        error => 'Invalid year '.param 'year'
+    }, 'error', request->content_type unless valid_year param 'year';
+    aggregate_projects {status => param 'result'}, param 'year';
+};
+
 
 get '/:id' => sub {
     my $id  = param 'id';

commit 12d85c84e9ee7067dc55819751ef0222b5e73ca1

Fixed project routes broken by be2ddbbbfe005af690f4ed8b000245390ea5de0b

lib/Pval/Routes/MajorProject.pm
@@ -36,12 +36,9 @@ get '/' => sub {
     aggregate_projects {};
 };
 
-get '/incoming' => sub {
-    aggregate_projects { status => 'pending' };
-};
-
 get '/:result' => sub {
-    aggregate_projects {status => param 'result'};
+    pass unless param 'result' ~~ qw/pending passed failed/;
+    aggregate_projects { status => param 'result' };
 };
 
 get '/year/:year' => sub {
@@ -60,6 +57,8 @@ get '/year/:year/:result' => sub {
 
 
 get '/:id' => sub {
+    pass unless param 'id' !~ /^\d+$/;
+
     my $id  = param 'id';
     my $db = schema;
     my $project = undef;