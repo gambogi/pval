f0554216663c30041828dbd402996f147a79d7bb
Matt Gambogi <m@gambogi.com>
2014-07-31 22:01:35 -0400

    Factored subroutines out of MajorProject.pm

--- lib/Pval/Routes/MajorProject.pm
+++ lib/Pval/Routes/MajorProject.pm
@@ -9,28 +9,11 @@ use Dancer::Plugin::Cache::CHI;
 use Dancer::Plugin::DBIC;
 use Pval::LDAP;
 use Pval::Misc;
+use Pval::Routes::MajorProject::Utils;
 use Try::Tiny;
 
 prefix '/projects';
 check_page_cache;
-
-sub aggregate_projects {
-    my $incoming = shift;
-    my $year = shift;
-    my $db = schema;
-    my $dtf = schema->storage->datetime_parser;
-
-    my @projects = $db->resultset('MajorProject')->search({
-        %$incoming,
-        date => {
-            -between => [ map { $dtf->format_datetime($_) } year_to_dates $year ],
-        },
-    }, { prefetch => 'submitter' });
-
-    return cache_page template_or_json({
-        projects => [ map { $_->json } @projects ]
-    }, 'user_project', request->content_type);
-}
 
 get '/' => sub {
     aggregate_projects {};
---
+++ lib/Pval/Routes/MajorProject/Utils.pm
@@ -0,0 +1,37 @@
+package Pval::Routes::MajorProject::Utils;
+
+use strict;
+use warnings;
+use v5.10;
+
+use Dancer ':syntax';
+use Dancer::Plugin::Cache::CHI;
+use Dancer::Plugin::DBIC;
+use Pval::LDAP;
+use Pval::Misc;
+use Try::Tiny;
+
+use base 'Exporter';
+our @EXPORT = qw/
+	aggregage_projects
+/;
+
+sub aggregate_projects {
+	my $incoming = shift;
+	my $year = shift;
+	my $db = schema;
+	my $dtf = schema->storage->datetime_parser;
+
+	my @projects = $db->resultset('MajorProject')->search({
+		%$incoming,
+		date => {
+			-between => [ map { $dtf->format_datetime($_) } year_to_dates $year ],
+		},
+	}, { prefetch => 'submitter' });
+
+	return cache_page template_or_json({
+		projects => [ map { $_->json } @projects ]
+	}, 'user_project', request->content_type);
+}
+
+1;
