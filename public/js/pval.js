e77eac1545c0559ab5586c826c0ef0af661ff89b
Matt Gambogi <m@gambogi.com>
2014-08-10 09:27:40 -0400

    put resources

---
+++ public/js/pval.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $("#project-form").submit(function(e) {
+        var formObj = $(this);
+        var formURL = "/projects/create";
+        $.ajax({
+            url: formURL,
+            type:
+
+    });
+});
